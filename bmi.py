6760b5ecc36b3e7d939604898296e315cfbb1f6e
wiwipeng <95857611+wiwipeng@users.noreply.github.com>
2021-12-09 20:29:24 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -6,10 +6,10 @@ if cofirm_input:
    bmi = w/(h*h)
    st.write('BMI為', bmi)
    elif (bmi < 18):
-        st.write('體重過輕')
+      st.write('體重過輕')
    elif (bmi < 24):
-        st.write('體重正常')
+      st.write('體重正常')
    elif (bmi < 27):
-        st.write('體重過重')
+      st.write('體重過重')
    else:
-        st.write('體重肥胖')
+      st.write('體重肥胖')
